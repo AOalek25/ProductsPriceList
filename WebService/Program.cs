3204ded411720ab4cf4d816d70858bbe308652f7
Ovchinnikov_AlA <Ovchinnikov_AlA@directum.ru>
2022-10-19 16:54:14 +0400

    Добавил странички для создания записи и upload/download файла.

--- WebService/Program.cs
+++ WebService/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<ProductsRepo<IProduct>>();
+builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
